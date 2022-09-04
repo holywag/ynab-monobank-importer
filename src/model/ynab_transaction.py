3135160ed3f419b82a4894788973aa8ce569a0bc
Maksym Lopatin <holywag@gmail.com>
2022-09-04 21:26:35 +0300

    Add import_id YNAB transaction field

--- src/model/ynab_transaction.py
+++ src/model/ynab_transaction.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 YnabTransaction = dict
 
+# Modify this value for debugging purpose
+IMPORT_ID_PREFIX = '1_'
+
 class YnabTransactionConverter:
     """Convert MonobankStatement object to the format of YNAB transaction
     """
@@ -22,6 +25,7 @@ class YnabTransactionConverter:
             'date': datetime.fromtimestamp(int(monobank_statement.time)).date(),
             'amount': monobank_statement.amount*10,
             'payee_name': monobank_statement.payee,
+            'payee_id': payee_id,
             'category_id': category_id,
             'memo': not category_id and not payee_id and f'mcc: {monobank_statement.mcc} description: {monobank_statement.description}' or None,
-            'payee_id': payee_id })
+            'import_id': f'{IMPORT_ID_PREFIX}{monobank_statement.id}' })
