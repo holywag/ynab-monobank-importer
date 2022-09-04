e73c8356961c0529fa351e58550403a19340ffe2
Maksym Lopatin <holywag@gmail.com>
2022-09-04 17:31:36 +0300

    Expose the "description" field of monobank statements

--- src/filters/cancel_filter.py
+++ src/filters/cancel_filter.py
@@ -9,20 +9,20 @@ class CancelFilter:
     cancel_prefix = 'Скасування. '
 
     def __init__(self, original_statement_set):
-        # Need to iterate over all ORIGINAL statements because the only criteria of 
-        # identification of cancel statements is the ORIGINAL payee name prefix
+        # Need to iterate over ALL statements because we need to match pairs
+        # of cancel/cancelled statements - the number of each ones may differ
         cancel, other = [], defaultdict(list)
         for s in original_statement_set:
-            payee_name = s['description']
+            payee_name = s.description
             if payee_name.startswith(self.cancel_prefix):
                 cancel.append(s)
             else:
-                other[f'{payee_name}_{s["amount"]}'].append(s)
+                other[f'{payee_name}_{s.amount}'].append(s)
         self.skip_statements = set()
         for s in cancel:
             try:
-                original_transaction = other[f'{s["description"][len(self.cancel_prefix):]}_{-s["amount"]}'].pop()
-                self.skip_statements.update({s['id'], original_transaction['id']})
+                original_transaction = other[f'{s.description[len(self.cancel_prefix):]}_{-s.amount}'].pop()
+                self.skip_statements.update({s.id, original_transaction.id})
             except IndexError:
                 # Ok, caught in case if the corresponding expense statement is not found in the original transaction set
                 pass
