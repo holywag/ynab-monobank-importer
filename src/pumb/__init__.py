4afb359c6fab3b1156ea6b84d1c194c2489169f4
Maksym Lopatin <holywag@gmail.com>
2023-11-19 11:30:53 +0200

    Drop duplicates from pumb reports. Parse multiple pdf documents.

--- src/pumb/__init__.py
+++ src/pumb/__init__.py
@@ -9,6 +9,7 @@ from collections.abc import Iterable
 
 # todo: support other currencies?
 # todo: use original column names for better integrity
+# todo: determine the real timezone
 
 class Api(BankApi):
     def __init__(self, conf: BankApiConfiguration):
@@ -19,20 +20,22 @@ class Api(BankApi):
         account = self.accounts.get(iban)
         if not account:
             raise UnknownIban(self.conf.name, iban)
-        filepath = Path(self.conf.token) / account.iban / 'Виписка по рахунку.pdf'
-        df = tabula.read_pdf(filepath, pages='all', lattice=True, multiple_tables=False)[0]
-        df.columns = ['date', 'amount_orig', 'date_processed', 'amount_uah', 'commission', 'card_num', 'description', 'type']
-        df.drop(df.tail(4).index, inplace=True) # drop summary
-        
-        # Merge rows that may be occasionally splitted between pages
-        df = df.groupby((~df['amount_uah'].isna()).cumsum()).agg(
-            lambda i: '\r'.join(map(str, filter(lambda j: not pd.isna(j), i))))
-
-        tr = iter(df.apply(lambda row: Transaction(
-            time=datetime.strptime(row.date, '%Y-%m-%d\r%H:%M:%S'),
+        df = pd.concat(self.__parse_pdf(f) for f in (Path(self.conf.token) / account.iban).rglob('*.pdf'))
+        if len(df) == 0:
+            return []
+        df.drop_duplicates(df.columns.difference(['description']), inplace=True)
+        df = df.apply(axis=1, result_type='reduce', func=lambda row: Transaction(
+            time=datetime.strptime(row.date, '%Y-%m-%d\r%H:%M:%S').astimezone(start.tzinfo),
             amount=int(float(re.match(r'(-?\d+\.\d{2})(?: UAH)', row.amount_uah).group(1)) * (100 if row.type == 'Надходження' else -100)),
             description=' '.join(re.split(r'\s+', row.description)),
-            account=account
-        ), axis=1, result_type='reduce'))
+            account=account))
+        return filter(lambda t: start <= t.time <= end, df)
 
-        return filter(lambda t: start <= t.time <= end, tr)
+    def __parse_pdf(self, filepath: Path) -> pd.DataFrame:
+        print(f'Parsing {filepath}')
+        df = tabula.read_pdf(filepath, pages='all', lattice=True, multiple_tables=False)[0]
+        df.columns = ['date', 'amount_orig', 'date_posted', 'amount_uah', 'commission', 'card_num', 'description', 'type']
+        df.drop(df.tail(4).index, inplace=True) # Drop summary
+        # Merge rows that may be occasionally splitted between pages
+        return df.groupby((~df['amount_uah'].isna()).cumsum(), as_index=False).agg(
+            lambda i: '\r'.join(map(str, filter(lambda j: not pd.isna(j), i))))
