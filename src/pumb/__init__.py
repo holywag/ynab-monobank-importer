47202a490085e46d91b82286659794c64d3f2ea5
Maksym Lopatin <stands-06.paint@icloud.com>
2023-11-19 20:18:08 +0200

    Fix duplicate detection in pumb reports

--- src/pumb/__init__.py
+++ src/pumb/__init__.py
@@ -23,7 +23,7 @@ class Api(BankApi):
         df = pd.concat(self.__parse_pdf(f) for f in (Path(self.conf.token) / account.iban).rglob('*.pdf'))
         if len(df) == 0:
             return []
-        df.drop_duplicates(df.columns.difference(['description']), inplace=True)
+        df.drop_duplicates(['date', 'amount_uah'], keep='last', inplace=True)
         df = df.apply(axis=1, result_type='reduce', func=lambda row: Transaction(
             time=datetime.strptime(row.date, '%Y-%m-%d\r%H:%M:%S').astimezone(start.tzinfo),
             amount=int(float(re.match(r'(-?\d+\.\d{2})(?: UAH)', row.amount_uah).group(1)) * (100 if row.type == 'Надходження' else -100)),
